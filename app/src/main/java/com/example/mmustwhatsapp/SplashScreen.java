bd74da836cb22d5b67127f8da2fd9e6170a4fd1b
Samora Machel <samora@me.laptop>
2022-03-02 18:35:58 +0300

    Fixed the splash screen bug

--- app/src/main/java/com/example/mmustwhatsapp/SplashScreen.java
+++ app/src/main/java/com/example/mmustwhatsapp/SplashScreen.java
@@ -5,13 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
 
 
 
@@ -21,9 +21,11 @@ public class SplashScreen extends AppCompatActivity {
                    public void run() {
                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                        startActivity(intent);
+                       finish();
                    }
                },
                1000
        );
     }
+
 }
